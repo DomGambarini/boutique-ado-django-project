c87313b868195d191b9a4fdf88283caab87da70d
Dominic Gambarini <domgambarini@gmail.com>
2024-03-03 15:46:25 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,6 +182,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Cache control
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
 
 # Bucket config
 AWS_STORAGE_BUCKET_NAME = 'boutique-ado-dominic'
